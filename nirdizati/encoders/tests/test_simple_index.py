bce8f0f11e07a35a539679715b49c69d5b93f636
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2017-10-22 16:46:48 +0300

    Add tests for BooleanEncoder

--- nirdizati/encoders/tests/test_simple_index.py
+++ nirdizati/encoders/tests/test_simple_index.py
@@ -5,22 +5,25 @@ from nirdizati.encoders import SimpleIndexEncoder
 
 
 class TestSimpleIndex(TestCase):
+    def setUp(self):
+        self.frame = data_frame()
+
     def test_has_columns(self):
         encoder = SimpleIndexEncoder()
-        df = encoder.encode_trace(data_frame())
+        df = encoder.encode_trace(self.frame)
         # Column check
-        self.assertTrue("case_id" in df.columns.values)
-        self.assertTrue("event_nr" in df.columns.values)
-        self.assertTrue("remaining_time" in df.columns.values)
-        self.assertTrue("elapsed_time" in df.columns.values)
-        self.assertTrue("prefix_1" in df.columns.values)
+        self.assertIn("case_id", df.columns.values)
+        self.assertIn("event_nr", df.columns.values)
+        self.assertIn("remaining_time", df.columns.values)
+        self.assertIn("elapsed_time", df.columns.values)
+        self.assertIn("prefix_1", df.columns.values)
         self.assertEqual(5, df.columns.size)
 
     def test_prefix_length(self):
         encoder = SimpleIndexEncoder()
-        df = encoder.encode_trace(data_frame(), 3)
+        df = encoder.encode_trace(self.frame, 3)
         # Column check
-        self.assertTrue("prefix_1" in df.columns.values)
-        self.assertTrue("prefix_2" in df.columns.values)
-        self.assertTrue("prefix_3" in df.columns.values)
+        self.assertIn("prefix_1", df.columns.values)
+        self.assertIn("prefix_2", df.columns.values)
+        self.assertIn("prefix_3", df.columns.values)
         self.assertEqual(7, df.columns.size)
