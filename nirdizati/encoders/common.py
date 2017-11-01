78c117e8b91cb5acccb871bf8279ccf4e60fb2d8
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2017-11-01 23:52:53 +0200

    Switch to sample_trace for testing

--- nirdizati/encoders/common.py
+++ nirdizati/encoders/common.py
@@ -14,17 +14,17 @@ def encode_boolean_frequency(data, encoding='boolean'):
 
     i = 0
     for case_id in case_ids:
-        df = data[data['case_id'] == case_id]
-        for j in range(0, max(df['event_nr'])):
+        case = data[data['case_id'] == case_id]
+        for j in range(0, max(case['event_nr'])):
             case_data = []
             event_length = j + 1
             for event in events:
-                case_data.append(__append_item(df, event, event_length, encoding))
+                case_data.append(__append_item(case, event, event_length, encoding))
             case_data.append(case_id)
             case_data.append(event_length)
-            remaining_time = calculate_remaining_time(df, event_length)
+            remaining_time = calculate_remaining_time(case, event_length)
             case_data.append(remaining_time)
-            elapsed_time = calculate_elapsed_time(df, event_length)
+            elapsed_time = calculate_elapsed_time(case, event_length)
             case_data.append(elapsed_time)
             encoded_data.loc[i] = case_data
             i = i + 1
